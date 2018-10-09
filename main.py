aa0ca41a1e049fab1e15734095cac0388543f729
HolidayMan <37782099+HolidayMan@users.noreply.github.com>
2018-10-09 22:44:27 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import telebot
+import constants
+import re
+
+bot = telebot.TeleBot(constants.token)
+
+@bot.message_handler(commands=['help'])
+def help(mess):
+	bot.send_message(mess.chat.id, 'Я бот Лёхи')
+
+bad_words = constants.bad_words
+
+@bot.message_handler(content_types=['text'])
+def gl(mess):
+	message = filter(None, re.split("[, \-!?:]+", mess.text))
+	for i in message:
+		if i.lower() in bad_words:
+			bot.delete_message(mess.chat.id, mess.message_id)
+	#else:
+	#	if mess.text.lower() == 'a' or mess.text.lower() == 'а':
+	#		if mess.text.lower() == 'а':
+	#			bot.send_message(mess.chat.id, 'б')
+	#	elif mess.text.lower() == 'b' or mess.text.lower() == 'б':
+	#		bot.send_message(mess.chat.id, 'a')
+	#	else:
+	#		bot.send_message(mess.chat.id, 'Ты не умеешь играть в эту игру(')
+
+
+if __name__ == '__main__':
+	bot.polling(none_stop=True)
