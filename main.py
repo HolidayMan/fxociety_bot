81a56cea22aed9fe1ed756783c1991fca397e479
HolidayMan <37782099+HolidayMan@users.noreply.github.com>
2018-10-12 22:01:10 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,62 +1,41 @@
-<<<<<<< HEAD
 import telebot
 import constants
 import re
 
 bot = telebot.TeleBot(constants.token)
 
-@bot.message_handler(commands=['help'])
-def help(mess):
-	bot.send_message(mess.chat.id, 'Я бот Лёхи')
+@bot.message_handler(commands=['start'])
+def start(mess):
+	bot.send_message(mess.chat.id, 'Привет! Я помогаю фильтровать маты в этом чате! \n Также у меня доступны такие команды: \n\
+	/help; \n /petard. \n В планах добавить бомбу. Но то потом)')
 
-bad_words = constants.bad_words
-
-@bot.message_handler(content_types=['text'])
-def gl(mess):
-	message = filter(None, re.split("[, \-!?:]+", mess.text))
-	for i in message:
-		if i.lower() in bad_words:
-			bot.delete_message(mess.chat.id, mess.message_id)
-	#else:
-	#	if mess.text.lower() == 'a' or mess.text.lower() == 'а':
-	#		if mess.text.lower() == 'а':
-	#			bot.send_message(mess.chat.id, 'б')
-	#	elif mess.text.lower() == 'b' or mess.text.lower() == 'б':
-	#		bot.send_message(mess.chat.id, 'a')
-	#	else:
-	#		bot.send_message(mess.chat.id, 'Ты не умеешь играть в эту игру(')
-
-
-if __name__ == '__main__':
-=======
-import telebot
-import constants
-import re
-
-bot = telebot.TeleBot(constants.token)
 
 @bot.message_handler(commands=['help'])
 def help(mess):
 	bot.send_message(mess.chat.id, 'Я бот Лёхи')
 
-bad_words = constants.bad_words
+@bot.message_handler(commands=['petard'])
+def petard(mess):
+	i = 1
+	while True:
+		try:
+			bot.delete_message(mess.chat.id, mess.message_id - i)
+			bot.delete_message(mess.chat.id, mess.message_id)
+			break
+		except telebot.apihelper.ApiException:
+			i+=1
 
+bad_words = constants.bad_words
+bad_words1 = constants.bad_words1
 @bot.message_handler(content_types=['text'])
-def gl(mess):
+def filter_word(mess):
 	message = filter(None, re.split("[, \-!?:]+", mess.text))
 	for i in message:
 		if i.lower() in bad_words:
 			bot.delete_message(mess.chat.id, mess.message_id)
-	#else:
-	#	if mess.text.lower() == 'a' or mess.text.lower() == 'а':
-	#		if mess.text.lower() == 'а':
-	#			bot.send_message(mess.chat.id, 'б')
-	#	elif mess.text.lower() == 'b' or mess.text.lower() == 'б':
-	#		bot.send_message(mess.chat.id, 'a')
-	#	else:
-	#		bot.send_message(mess.chat.id, 'Ты не умеешь играть в эту игру(')
-
+	if mess.text.lower() in bad_words1:
+		bot.delete_message(mess.chat.id, mess.message_id)
 
 if __name__ == '__main__':
->>>>>>> e9354c8b0f90fd56f7c86c2a7b64de6d835f69f5
+	print('Bot started')
 	bot.polling(none_stop=True)
