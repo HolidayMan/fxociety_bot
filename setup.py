fd53246cc4f28c86d922d7f28bc269ad72f27f0e
HolidayMan <37782099+HolidayMan@users.noreply.github.com>
2018-11-24 22:42:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ def filter_word(mess):
 if __name__ == '__main__':
 	print('Bot started')
 	while True:
-	try:
-		bot.polling(none_stop=True, timeout=0.02)
-	except requests.exceptions.ConnectionError:
-		time.sleep(15)
+		try:
+			bot.polling(none_stop=True, timeout=0.02)
+		except requests.exceptions.ConnectionError:
+			time.sleep(15)
