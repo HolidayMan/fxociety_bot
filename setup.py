4a967c19ce2f21e420f94423142795c38d54db12
HolidayMan <37782099+HolidayMan@users.noreply.github.com>
2018-12-25 20:20:41 +0200

    added polling

--- setup.py
+++ setup.py
@@ -57,3 +57,12 @@ def filter_word(mess):
 				return 0
 			except telebot.apihelper.ApiException:
 				return 0
+if __name__ == '__main__':
+	print('Bot started')
+	while True:
+		try:
+			bot.polling(none_stop=True,interval=0)
+		except:
+			print('[-] Bot is sleeping')
+			time.sleep(15)
+			print('[-] Bot is working')
